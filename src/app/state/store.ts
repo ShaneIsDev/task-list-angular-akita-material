0d715253146991071800784bb4fc74788547555c
Shane Brown <buithinh.tg@gmail.com>
2022-05-18 17:52:11 +0700

    finished with the store.ts

---
+++ src/app/state/store.ts
@@ -0,0 +1,30 @@
+import { Task } from "../task.model";
+import { Store, StoreConfig } from "@datorama/akita";
+import { Injectable } from "@angular/core";
+export interface TaskState{
+    tasks: Task[];
+    isLoaded: boolean;
+}
+
+
+
+export const getInitialState = () =>{
+    return {
+        tasks:[],
+        isLoaded:false,
+    };
+};
+
+@Injectable({
+    providedIn:'root'
+})
+@StoreConfig({name: 'task'})
+
+export class TaskStore extends Store<TaskState>{
+
+    constructor(){
+        super(getInitialState());
+
+
+    }
+}
