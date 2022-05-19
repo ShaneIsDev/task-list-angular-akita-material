1ef39b207257fd939873f5f0cd41044e02c4b8f7
Shane Brown <buithinh.tg@gmail.com>
2022-05-19 09:29:14 +0700

    added query.ts

---
+++ src/app/state/query.ts
@@ -0,0 +1,22 @@
+import { Query } from "@datorama/akita";
+import { Observable } from "rxjs";
+import { TaskStore, TaskState } from "./store";
+import { Task } from "../task.model";
+
+
+
+export class TaskQuery extends Query <TaskState>{
+    constructor(private  taskStore:TaskStore){
+        super(taskStore);
+    }
+
+    getTask(): Observable<Task[]>{
+        return this.select(state=>state.tasks);
+    }
+
+    getLoaded():Observable<boolean>{
+        return this.select(state=>state.isLoaded);
+    }
+
+    
+}
