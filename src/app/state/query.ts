84b6091f85eb7db7b75e9b3819b0cbdd3aab0c21
Shane Brown <buithinh.tg@gmail.com>
2022-05-19 09:31:51 +0700

    completed query and store

--- src/app/state/query.ts
+++ src/app/state/query.ts
@@ -5,18 +5,20 @@ import { Task } from "../task.model";
 
 
 
-export class TaskQuery extends Query <TaskState>{
-    constructor(private  taskStore:TaskStore){
+export class TaskQuery extends Query<TaskState>{
+    constructor(private taskStore: TaskStore) {
         super(taskStore);
     }
 
-    getTask(): Observable<Task[]>{
-        return this.select(state=>state.tasks);
+    getTask(): Observable<Task[]> {
+        return this.select(state => state.tasks);
     }
 
-    getLoaded():Observable<boolean>{
-        return this.select(state=>state.isLoaded);
+    getLoaded(): Observable<boolean> {
+        return this.select(state => state.isLoaded);
     }
 
-    
+    getLoading(): Observable<boolean> {
+        return this.selectLoading();
+    }
 }
--- src/app/state/store.ts
+++ src/app/state/store.ts
@@ -1,28 +1,28 @@
 import { Task } from "../task.model";
 import { Store, StoreConfig } from "@datorama/akita";
 import { Injectable } from "@angular/core";
-export interface TaskState{
+export interface TaskState {
     tasks: Task[];
     isLoaded: boolean;
 }
 
 
 
-export const getInitialState = () =>{
+export const getInitialState = () => {
     return {
-        tasks:[],
-        isLoaded:false,
+        tasks: [],
+        isLoaded: false,
     };
 };
 
 @Injectable({
-    providedIn:'root'
+    providedIn: 'root'
 })
-@StoreConfig({name: 'task'})
+@StoreConfig({ name: 'task' })
 
 export class TaskStore extends Store<TaskState>{
 
-    constructor(){
+    constructor() {
         super(getInitialState());
     }
 }
